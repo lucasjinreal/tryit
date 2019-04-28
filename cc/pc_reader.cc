e6e9bf252b0a7fd0b0e6c07004f21d0dd11e40de
Your Name <you@example.com>
2019-04-28 14:30:55 +0800

    add a tool

---
+++ cc/pc_reader.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+// #include <pcl_visualization/cloud_viewer.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include "thor/os.h"
+
+
+
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+  if (argc >= 2)
+  {
+    string pcd_f = string(argv[1]);
+    cout << pcd_f << endl;
+
+    string f_type = thor::os::suffix(pcd_f);
+    if (f_type == "pcd") {
+        // using pcd open it
+        if (pcl::io::loadPCDFile<pcl::PointXYZ>(pcd_f, *cloud) == -1) //* load the file
+        {
+        PCL_ERROR("Try using pcd_io open it but failed. \n");
+        return (-1);
+        }
+    } else if (f_type == "ply") {
+        // using ply open it, this may fail
+        if (pcl::io::loadPLYFile<pcl::PointXYZ>(pcd_f, *cloud) == -1) //* load the file
+        {
+        PCL_ERROR("Try using ply_io open it but failed. \n");
+        return (-1);
+        }
+    }
+    
+    std::cout << "Loaded "
+              << cloud->width * cloud->height
+              << " data points from test_pcd.pcd with the following fields: "
+              << std::endl;
+    // for (size_t i = 0; i < cloud->points.size(); ++i)
+    //   std::cout << "    " << cloud->points[i].x
+    //             << " " << cloud->points[i].y
+    //             << " " << cloud->points[i].z << std::endl;
+
+    // visualize it
+    pcl::visualization::CloudViewer viewer("ff");
+    viewer.showCloud(cloud);
+    while (!viewer.wasStopped())
+    {
+    }
+  }
+  else
+  {
+    cout << "you must provide pcd/ply file path.\n";
+  }
+  return (0);
+}
