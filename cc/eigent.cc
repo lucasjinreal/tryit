449eca1938a41d75d914942a8601e1957ef49ec2
Your Name <you@example.com>
2019-05-22 17:20:23 +0800

    add

--- cc/eigent.cc
+++ cc/eigent.cc
@@ -1,6 +1,9 @@
 #include <Eigen/Core>
 #include <Eigen/Eigen>
 #include <iostream>
+#include <queue>
+#include <deque>
+
 
 using namespace std;
 using namespace Eigen;
@@ -12,4 +15,14 @@ int main() {
     a << 0, 3, 5, 7, 6, 9;
 
     cout << a[4] << " " << a[3];
+
+    deque<Eigen::VectorXf> trace;
+    Eigen::VectorXf one_p(2);
+    one_p << 3, 44;
+    trace.push_back(one_p);
+    trace.push_back(one_p);
+    trace.push_front(one_p);
+    cout << "dont\n";
+    Eigen::VectorXf op = trace[1];
+    cout << op << endl;
 }
