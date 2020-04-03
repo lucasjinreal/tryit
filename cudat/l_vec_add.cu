8fafd7d5ce44751b2fc96886563e64f33eaa1a24
lucasjin <aa@qq.com>
2020-04-04 07:42:21 +0800

    add

---
+++ cudat/l_vec_add.cu
@@ -0,0 +1,39 @@
+/**
+ * 
+ * simple CUDA C++ file
+ * 
+ * 
+ * */
+#include <iostream>
+#include <cuda.h>
+
+using namespace std;
+
+__global__ void vec_add(int *a, int *b)
+{
+    int i = blockIdx.x;
+    a[i] += b[i];
+}
+
+int main()
+{
+    const int N = 100;
+    // ---- allocate the memory ---- //
+    int *a, *b;
+    cudaMallocManaged(&a, N*sizeof(int));
+    cudaMallocManaged(&b, N*sizeof(int));
+    // ---- initialize a,b ---- //
+    for(int i=0;i<N;i++)
+    {
+        a[i] = i;
+        b[i] = 2*i;
+    }
+    // ---- add b to a ---- //
+    vec_add<<<N,1>>>(a,b);
+    cudaDeviceSynchronize();
+    // ---- display a ---- //
+    for(int i=0;i<N;i++)
+    {
+        cout << a[i] << endl;
+    }
+}
