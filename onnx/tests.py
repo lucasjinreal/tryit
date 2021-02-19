7d9da0563bed0e461639f2d11ccdb4039233e470
FagangJin <fagangjin@autox.ai>
2021-02-19 14:28:12 +0800

    dynamic conv con not export for now

--- onnx/tests.py
+++ onnx/tests.py
@@ -141,26 +141,28 @@ class MG(nn.Module):
         super().__init__()
         # for test if torch.cat([bool, bool]) can convert
 
-    def forward(self, x):
-        kernel_preds = torch.randn([8, 4, 3, 3])
-        preds = F.conv2d(x, kernel_preds,
+    def forward(self, x, b):
+        # x, b = x
+        preds = F.conv2d(x, b,
                              stride=1)
-        preds = preds.to(torch.float)
-        preds = preds.sigmoid()
-        seg_masks = preds > 0.03
-        return seg_masks
+        # preds = preds.to(torch.float)
+        # preds = preds.sigmoid().float()
+        # seg_masks = preds > torch.tensor(0.03, dtype=torch.float)
+        # return seg_masks
+        return preds
 
 
 torch_model = MG()
 x = torch.randn([1, 4, 24, 24])
-torch_out = torch_model(x)
+b = torch.randn([8, 4, 3, 3])
+torch_out = torch_model(x, b)
 
 # Export the model
 torch.onnx.export(torch_model,               # model being run
-                  x,
+                  (x, b),
                   "a.onnx",
                   export_params=True,        # store the trained parameter weights inside the model file
-                  opset_version=11,          # the ONNX version to export the model to
+                  opset_version=12,          # the ONNX version to export the model to
                   do_constant_folding=True,
                   verbose=True)
 print('Done!')
